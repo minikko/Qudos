545dd0201d5061ec3b475053d45cbb332209df37
Jo-Nikko Liawan <nikko.qudos@gmail.com>
2020-04-20 08:21:49 +0800

    assets addition

--- gulpfile.js
+++ gulpfile.js
@@ -6,36 +6,6 @@ const csso = require('gulp-csso');
 const autoprefixer = require('gulp-autoprefixer');
 const sourcemap = require('gulp-sourcemaps');
 
-gulp.task('watch', () => {
-    gulp.watch('__assets/scss/**/*.scss', (done) => {
-        gulp.series(['clean', 'styles'])(done);
-    });
-});
-
-gulp.task('styles', () => {
-    return gulp.src('__assets/scss/**/*.scss')
-        // Initialize Sourcemap
-        .pipe(sourcemap.init())
-        // Compile SASS files
-        .pipe(sass().on('error', sass.logError))
-        // Autoprefix CSS styles for cross browser compatibility
-        .pipe(autoprefixer())
-        // Minify CSS
-        .pipe(csso())
-        // Rename output file
-        .pipe(rename('styles.min.css'))
-        // Write Sourcemap
-        .pipe(sourcemap.write('.'))
-        // Destination folder
-        .pipe(gulp.dest('__assets/css'));
-});
-
-gulp.task('clean', () => {
-    return del([
-        '__assets/css/main.css',
-    ]);
-});
-
 // CORE WATCH
 gulp.task('core__watch', () => {
     gulp.watch('core__assets/scss/**/*.scss', (done) => {
